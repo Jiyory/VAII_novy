ca5952fbf8129525eb5a5186dbb851a1cb2f9833
Jozef Marek <jozef161@gmail.com>
2020-12-06 12:50:35 +0100

    Priprava registracie

--- client/js/login.js
+++ client/js/login.js
@@ -29,6 +29,19 @@ app.controller("loginCTRL", function($scope, $sce, $templateRequest, $compile, m
         pass: "admin"
     };
 
+    $scope.registerData = {
+        mail: "jozef161@gmail.com",
+        pass: "a1s2d3f4",
+        pass2: "a1s2d3f4",
+        fname: "Gery",
+        lname: "Ardy",
+        comp: "Bez spolocnosti",
+        phone: ""
+    };
+
+    $scope.logErr = [true, false];
+    $scope.regErr = [true, false, true, false, true, false, true];
+
     /* Nacitanie html stranky */
     var loadAPI = $sce.getTrustedResourceUrl('client/components/modals/login.html');
     $templateRequest(loadAPI).then(function(template) {
@@ -48,6 +61,7 @@ app.controller("loginCTRL", function($scope, $sce, $templateRequest, $compile, m
             $scope.errorMessageLogin = "Nemozno sa prihlasit! Skontrolujte udaje.";
             return;
         }
+        $scope.errorMessageLogin = "Prihlasujem...";
         $scope.pData = $.param({
             mail: $scope.loginData.mail,
             pass: $scope.loginData.pass
@@ -59,6 +73,7 @@ app.controller("loginCTRL", function($scope, $sce, $templateRequest, $compile, m
             data: $scope.pData,
             headers: {'Content-Type': 'application/x-www-form-urlencoded'} // musi byt, inak to neposiela data
         }).then(function(value) {
+            //console.log(value.data);
             if (value.data.u_id != null) {
                 UserData.user.u_id = value.data.u_id;
                 UserData.user.email = value.data.email;
@@ -73,35 +88,104 @@ app.controller("loginCTRL", function($scope, $sce, $templateRequest, $compile, m
             } else {
                 $scope.errorMessageLogin = "Nepodarilo sa prihlasit! Skontrolujte udaje!";
             }
-                /*console.log($scope.data[$scope.modID - 1])
-                $scope.data[$scope.modID - 1] = {
-                    rot_id: $scope.modID,
-                    header: $scope.modHeader,
-                    text: $scope.modText,
-                    img: '<img src="client/design/rotate'+$scope.modID+'.jpg" alt="'+$scope.modHeader+'">',
-                }
-                $('#modalRotate').modal('hide');*/
         }, function() {});
     };
+
+    $scope.tryRegister = function() {
+        if ($scope.errorMessageRegister.length != 0) {
+            $scope.errorMessageRegister = "Nemozno sa zaregistrovat! Skontrolujte udaje.";
+            return;
+        }
+        $scope.errorMessageLogin = "Registrujem...";
+        $scope.pData = $.param({
+            mail: $scope.registerData.mail,
+            pass: $scope.registerData.pass,
+            fname: $scope.registerData.fname,
+            lname: $scope.registerData.lname,
+            comp: $scope.registerData.comp,
+            phone: $scope.registerData.phone,
+        });
+        $http({
+            method: 'POST',
+            url: 'server/register.php',
+            data: $scope.pData,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'} // musi byt, inak to neposiela data
+        }).then(function(value) {
+            console.log(value.data);
+            if (value.data == "true") {
+                
+            }
+        }, function() {});
+    }
 
     $scope.checkInputLogin = function() { // input type email nie je podporovany
         var pass = $scope.loginData.pass;
         var mail = $scope.loginData.mail;
         $scope.errorMessageLogin = "";
+        $scope.logErr = [false, false];
         if (pass.length < 5) {
             $scope.errorMessageLogin += "Heslo je príliš krátke! Minimálne 5 znakov.\n";
+            $scope.logErr[1] = true;
         }
         if (mail.length < 5 || (mail.match(/@/g) || []).length != 1) {
             $scope.errorMessageLogin += "Formát emailu nie je správny.\n";
+            $scope.logErr[0] = true;
+        }
+    };
+
+    $scope.checkInputRegister = function() {
+        console.log("sdgsdg");
+        var mail = $scope.registerData.mail;
+        var pass = $scope.registerData.pass;
+        var pass2 = $scope.registerData.pass2;
+        var fname = $scope.registerData.fname;
+        var lname = $scope.registerData.lname;
+        var comp = $scope.registerData.comp;
+
+        $scope.regErr = [false, false, false, false, false, false, false];
+        $scope.errorMessageRegister = "";
+
+        if (mail.length < 5 || (mail.match(/@/g) || []).length != 1) {
+            $scope.errorMessageRegister += "Formát emailu nie je správny.\n";
+            $scope.regErr[0] = true;
+        }
+        if (pass.length < 5) {
+            $scope.errorMessageRegister += "Heslo je príliš krátke [5].\n";
+            $scope.regErr[1] = true;
+        }
+        if (pass != pass2) {
+            $scope.errorMessageRegister += "Heslá sa nezhodujú.\n";
+            $scope.regErr[2] = true;
+        }
+        if (fname.length < 4) {
+            $scope.errorMessageRegister += "Meno je príliš krátke [4].\n";
+            $scope.regErr[3] = true;
+        }
+        if (fname[0] < 'A' || fname[0] > 'Z') {
+            $scope.errorMessageRegister += "Meno nezačína veľkým písmenom.\n";
+            $scope.regErr[3] = true;
+        }
+        if (lname.length < 3) {
+            $scope.errorMessageRegister += "Priezvisko je príliš krátke [3].\n";
+            $scope.regErr[4] = true;
+        }
+        if (lname[0] < 'A' || lname[0] > 'Z') {
+            $scope.errorMessageRegister += "Prezvisko nezačína veľkým písmenom.\n";
+            $scope.regErr[4] = true;
+        }
+        if (comp.length < 3) {
+            $scope.errorMessageRegister += "Spoločnosť je príliš krátka [3].\n";
+            $scope.regErr[5] = true;
         }
     };
 
     $scope.checkInputLogin();
+    $scope.checkInputRegister();
 });
 
 app.factory("modalLogin", function () {
     return {
-        showModal: false,
+        showModal: true,
 
         showLogin: function() {
             this.showModal = true;
--- server/login.php
+++ server/login.php
@@ -6,26 +6,31 @@
     $query->bindValue(':fullname', $search);
     */
     if (isset($_POST["mail"]) && isset($_POST["pass"])) {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "vpgum";
-        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //$sql = $conn->prepare("SELECT * FROM users");
-        $sql = $conn->prepare("SELECT * FROM users WHERE email=:mail");
-        $sql->bindValue(':mail', $_POST["mail"]);
-        $sql->execute();
-        $rows = $sql->fetch(PDO::FETCH_ASSOC);
-        //print_r($red);
-        //echo json_encode($rows);
-        if ($sql->rowCount() > 0) {
-            $toHash = $_POST["pass"].$rows["salt"];
-            $hash = hash('sha256', $toHash);
-            if ($hash == $rows["pass"]) {
-                header('Content-Type: application/json');
-                echo json_encode($rows);
+        try {
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $database = "vpgum";
+            $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //$sql = $conn->prepare("SELECT * FROM users");
+            $sql = $conn->prepare("SELECT * FROM users WHERE email=:mail");
+            $sql->bindValue(':mail', $_POST["mail"]);
+            $sql->execute();
+            $rows = $sql->fetch(PDO::FETCH_ASSOC);
+            //print_r($red);
+            //echo json_encode($rows);
+            if ($sql->rowCount() > 0) {
+                $toHash = $_POST["pass"].$rows["salt"];
+                $hash = hash('sha256', $toHash);
+                if ($hash == $rows["pass"]) {
+                    header('Content-Type: application/json');
+                    echo json_encode($rows);
+                }
             }
-        }
+            $conn = null;
+        } catch (PDOException $e) {
+            
+        } 
     }
 ?>
